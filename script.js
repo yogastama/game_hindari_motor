e86c734cb1f606fdf17cd1134042544400f05ada
Adam Alfiansyah <adamalfiansyah@gmail.com>
2019-09-29 18:02:11 +0700

    first publish

--- script.js
+++ script.js
@@ -3,23 +3,47 @@ let game;
 let orang;
 let motor = false;
 let durasi = 500;
-let timerId = false;
+let timer = false;
+let timerCollision = false;
+
+/*jarak dari ukuran gambar .jalan / .lompat dari kanan sampai kaki
+  supaya mati hanya ketika ketabrak kaki, bukan box nya
+  ini dilakukan karena kepala lebih maju dari kaki 
+	
+  aturLebarOrang akan menjadi dinamis agar bisa lebih dalam ketika melompat
+  supaya real seperti hanya mati ketika tabrak stang motor
+*/
+let aturLebarOrang = 40;
+
+function range(min, max){
+ 	/*	karena Javascript tidak punya fungsi range, maka kita buat range sendiri
+
+ 		math floor lakukan pembulatan angka
+ 		math random mendapatkan angka acak, contohnya : 1.28911
+	*/
+ 	durasi = Math.floor(Math.random() * (max-min+1) + min);
+
+ 	return durasi;
+}
 
 function pause(){
 	orang.addClass('pause');
 	motor.addClass('pause');
-	clearTimeout(timerId);
+	clearTimeout(timer);
+	clearTimeout(timerCollision);
 }
 
 function jalan(){	
 	intervalMotor();
-
 	orang.removeClass('pause');
 	
 	//cek lagi ada motor atau gak
 	if(motor !== false){
 		motor.removeClass('pause');
 	}
+
+	//delay sebelum mulai menghitung collision
+	setTimeout(collision, 1000);
 }
 
 function hapusMotor(){
@@ -32,46 +56,82 @@ function hapusMotor(){
 function lompat(){
 	orang.addClass('lompat');
 
-	//belum FIX
-	hapusMotor();
-
 	//berhenti lompat
 	orang.on("animationend", function() {
 	    $(this).removeClass("lompat");
 	});
 }
 
+function collision(){
+	setTimeout(function(){
+		motor.each(function(index, obj){
+			//orang
+			let posOrang = { 'lebar'	: orang.width(),
+							 'tinggi'	: orang.height(),
+							 'x'		: orang.offset().left,
+							 'y'		: orang.offset().top };
+			
+			//motor
+			let posMotor = { 'lebar'	: $(this).width(),
+							 'tinggi'	: $(this).height(),
+							 'x'		: $(this).offset().left,
+							 'y'		: $(this).offset().top };
+
+			// console.log((posOrang.y + posOrang.tinggi) +'--'+ posMotor.y);
+			// console.log((posOrang.x + posOrang.lebar) +' -- '+ posMotor.x);
+
+
+			//50 karena kepala lebih besar dari badan, padahal yang di tabrak badan, jadi kurangin 50
+			let lebarOrang = posOrang.x + posOrang.lebar - aturLebarOrang;
+			let lebarMotor = posMotor.x + posMotor.lebar;
+			
+			let tinggiOrang = posOrang.y + posOrang.tinggi;
+			let tinggiMotor = posMotor.y + posMotor.tinggi;
+
+			if(lebarOrang > posMotor.x && lebarOrang < lebarMotor){
+
+				if(tinggiOrang < tinggiMotor){
+					// console.log(lebarOrang + '--' + posMotor.x + ' && ' + lebarOrang + '--' + lebarMotor);
+					// pause();
+					// console.log('%cSuccess', 'color:green');
+					hapusMotor();
+				}else{
+					// console.log(lebarOrang + '--' + posMotor.x + ' && ' + lebarOrang + '--' + lebarMotor);
+					// console.log('%cgagal', 'color:red');
+					// pause();
+					orang.addClass('mati');
+				}
+			}
+		});
+
+		collision();
+	}, 100);
+}
+
 function bikinMotor(){
+	//bikin emelement motor
 	game.prepend('<div class="motor">');
+
+	//isi variable motor sebagai selector
 	motor = $('.motor');
 
 	//animasi motor
-	motor.css({'animation' : 'maju 20s forwards,  motor2 .3s steps(3) infinite'});
-}
-
-function range(min, max){
- 	/*	karena Javascript tidak punya fungsi range, maka kita buat range sendiri
-
- 		math floor lakukan pembulatan angka
- 		math random mendapatkan angka acak, contohnya : 1.28911
-	*/
- 	durasi = Math.floor(Math.random() * (max-min) + min);
-
- 	return durasi;
+	let jeniMotor = range(1, 3);
+	let pilihMotor = 'motor' + jeniMotor;
+	motor.first().addClass(pilihMotor);
+	motor.first().css({'animation' : 'maju 18s forwards,  '+ pilihMotor +' .3s steps(3) infinite'});
 }
 
 function intervalMotor(){
-	timerId = setTimeout(function(){
+	timer = setTimeout(function(){
 				 	//bikin motor
 				 	bikinMotor();
 				  	
 				  	//clear timeout saatini
-				  	clearTimeout(timerId);
+				  	clearTimeout(timer);
 				 	
 				 	//atur durasi keluar motor antara 3 detik sampai 6 detk
 				 	durasi = range(3000, 5000);
-				 	// durasi = 3000;
-				 	console.log(durasi);
 
 				  	intervalMotor();
 			  }, durasi);
@@ -85,12 +145,6 @@ function tombol(){
 			if(orang.hasClass('pause')){
 				//pertama kali spasi jalan
 				jalan(); 
-
-				//pertama kali mulai remove class start
-				if(game.hasClass('start')){
-					game.removeClass('start');
-				}
-
 			}else{
 				//spasi selanjutnya lompat
 				lompat();
@@ -112,9 +166,6 @@ $(document).ready(function(){
 	//simpan selector di global variable
 	game = $('#game');
 	orang = $('#orang');
-
-	//awal game, tambahin class start
-	game.addClass('start');
 
 	init();
 });
